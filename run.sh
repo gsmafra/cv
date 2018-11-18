b369f97b279f92c649be4eeaf83cb1288e5b9b29
gsmafra <gsenamafra@gmail.com>
2018-11-18 00:23:12 -0200

    add recent experience

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 wget https://github.com/gsmafra/cv-en-2p/archive/master.zip
 unzip master.zip
 cd cv-en-2p-master
-xelatex cv.tex
-curl --upload-file cv.pdf https://transfer.sh/cv.pdf
+xelatex -output-directory=output src/cv.tex
+curl --upload-file output/cv.pdf https://transfer.sh/cv.pdf
